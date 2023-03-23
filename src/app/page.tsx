e10f067ca3734a901c52892eaacb9dbc4fe76da5
Dani <chisteleco@gmail.com>
2023-03-23 14:12:48 +0100

    change main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,145 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
-
+import './tailwindWrapperCSS.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <div >
+       <h1 className="text-3xl font-bold">Tabla de puntuaciones</h1>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
+        <br></br>
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className="grid grid-cols-4 gap-4">
+       {/* GALLO APUESTAS */}
+         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <label className="text-2xl font-bold">Gallo</label>
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th className="px-6 py-3">Personaje</th>
+                <th className="px-6 py-3">Muerto</th>
+                <th className="px-6 py-3">Puntos</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Personaje1 </td>
+                <td>No</td>
+                <td>0</td>
+              </tr>
+              <tr>
+              <td>Personaje2 </td>
+                <td>No</td>
+                <td>0</td>
+              </tr>
+              <tr>
+              <td>Personaje3 </td>
+                <td>No</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+          </table>
+          <label className="text-1xl font-bold">Total: 0</label>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-        
+{/* LIDIA APUESTAS  */}
+   
+       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <label className="text-2xl font-bold">Lidia</label>
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th className="px-6 py-3">Personaje</th>
+                <th className="px-6 py-3">Muerto</th>
+                <th className="px-6 py-3">Puntos</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Personaje1 </td>
+                <td>No</td>
+                <td>0</td>
+              </tr>
+              <tr>
+              <td>Personaje2 </td>
+                <td>No</td>
+                <td>0</td>
+              </tr>
+              <tr>
+              <td>Personaje3 </td>
+                <td>No</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+          </table>
+          <label className="text-1xl font-bold">Total: 0</label>
+          </div>
+       {/* MARTA APUESTAS */}
+       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <label className="text-2xl font-bold">Marta</label>
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th className="px-6 py-3">Personaje</th>
+                <th className="px-6 py-3">Muerto</th>
+                <th className="px-6 py-3">Puntos</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Personaje1 </td>
+                <td>No</td>
+                <td>0</td>
+              </tr>
+              <tr>
+              <td>Personaje2 </td>
+                <td>No</td>
+                <td>0</td>
+              </tr>
+              <tr>
+              <td>Personaje3 </td>
+                <td>No</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+          </table>
+          <label className="text-1xl font-bold">Total: 0</label>
+          </div>
+                 {/* CARMEN APUESTAS */}
+         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <label className="text-2xl font-bold">Carmen</label>
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th className="px-6 py-3">Personaje</th>
+                <th className="px-6 py-3">Muerto</th>
+                <th className="px-6 py-3">Puntos</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Personaje1 </td>
+                <td>No</td>
+                <td>0</td>
+              </tr>
+              <tr>
+              <td>Personaje2 </td>
+                <td>No</td>
+                <td>0</td>
+              </tr>
+              <tr>
+              <td>Personaje3 </td>
+                <td>No</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+          </table>
+          <label className="text-1xl font-bold">Total: 0</label>
+          </div>
+</div>
       </div>
     </main>
   )
